a4e17c0adfc5c5bfe036691713520173699552cf
Gabbfernyh <bielmgfv.python@gmail.com>
2025-09-23 22:45:33 -0300

    ultima atualização

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,35 +1,29 @@
 function showWelcomeAlert(message) {
-    const hasAccessed = localStorage.getItem('firstAccess');
+    const alertContainer = document.createElement('div');
+    alertContainer.className = 'first-access-alert-container';
 
-    if (!hasAccessed) {
-        const alertContainer = document.createElement('div');
-        alertContainer.className = 'first-access-alert-container';
+    const alertContent = document.createElement('div');
+    alertContent.className = 'first-access-alert-content';
+    alertContent.innerHTML = `
+        <p>${message}</p>
+        <button class="close-alert-btn">X</button>
+    `;
 
-        const alertContent = document.createElement('div');
-        alertContent.className = 'first-access-alert-content';
-        alertContent.innerHTML = `
-            <p>${message}</p>
-            <button class="close-alert-btn">X</button>
-        `;
+    alertContainer.appendChild(alertContent);
+    document.body.appendChild(alertContainer);
 
-        alertContainer.appendChild(alertContent);
-        document.body.appendChild(alertContainer);
+    // Força o navegador a aplicar o estilo inicial antes de adicionar a classe 'show'
+    setTimeout(() => {
+        alertContainer.classList.add('show');
+    }, 10);
 
-        // Força o navegador a aplicar o estilo inicial antes de adicionar a classe 'show'
+    const closeButton = alertContent.querySelector('.close-alert-btn');
+    closeButton.addEventListener('click', () => {
+        alertContainer.classList.remove('show');
         setTimeout(() => {
-            alertContainer.classList.add('show');
-        }, 10);
-
-        const closeButton = alertContent.querySelector('.close-alert-btn');
-        closeButton.addEventListener('click', () => {
-            alertContainer.classList.remove('show');
-            setTimeout(() => {
-                document.body.removeChild(alertContainer);
-            }, 500); // Deve ser igual ao tempo da transição no CSS
-        });
-
-        localStorage.setItem('firstAccess', 'true');
-    }
+            document.body.removeChild(alertContainer);
+        }, 500); // Deve ser igual ao tempo da transição no CSS
+    });
 }
 
 function showCustomAlert(message) {
