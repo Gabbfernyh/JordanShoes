db0356cc337a562a8b671a1d18a13dbbd83b4b0e
Gabbfernyh <bielmgfv.python@gmail.com>
2025-09-23 22:30:22 -0300

    .

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,29 +1,3 @@
-function showCustomAlert(message) {
-    // Cria o elemento do alerta
-    const alertElement = document.createElement('div');
-    alertElement.className = 'custom-alert';
-    alertElement.textContent = message;
-
-    // Adiciona o alerta ao body
-    document.body.appendChild(alertElement);
-
-    // Força o navegador a aplicar o estilo inicial antes de adicionar a classe 'show'
-    setTimeout(() => {
-        alertElement.classList.add('show');
-    }, 10);
-
-    // Remove o alerta depois de 3 segundos
-    setTimeout(() => {
-        alertElement.classList.remove('show');
-        
-        // Espera a transição de saída terminar para remover o elemento
-        setTimeout(() => {
-            document.body.removeChild(alertElement);
-        }, 500); // Deve ser igual ao tempo da transição no CSS
-
-    }, 3000);
-}
-
 function showWelcomeAlert(message) {
     const hasAccessed = localStorage.getItem('firstAccess');
 
@@ -56,4 +30,30 @@ function showWelcomeAlert(message) {
 
         localStorage.setItem('firstAccess', 'true');
     }
+}
+
+function showCustomAlert(message) {
+    // Cria o elemento do alerta
+    const alertElement = document.createElement('div');
+    alertElement.className = 'custom-alert';
+    alertElement.textContent = message;
+
+    // Adiciona o alerta ao body
+    document.body.appendChild(alertElement);
+
+    // Força o navegador a aplicar o estilo inicial antes de adicionar a classe 'show'
+    setTimeout(() => {
+        alertElement.classList.add('show');
+    }, 10);
+
+    // Remove o alerta depois de 3 segundos
+    setTimeout(() => {
+        alertElement.classList.remove('show');
+        
+        // Espera a transição de saída terminar para remover o elemento
+        setTimeout(() => {
+            document.body.removeChild(alertElement);
+        }, 500); // Deve ser igual ao tempo da transição no CSS
+
+    }, 3000);
 }
